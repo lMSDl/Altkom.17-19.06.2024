5d09bc35ec9963455e0024df207baadfbb63ec85
Paweł Łęczycki <pawel@msd.info.pl>
2024-06-17 10:55:22 +0200

    KISS

---
+++ WPC/DesignPrinciples/PaymentAccount.cs
@@ -0,0 +1,22 @@
+﻿
+namespace WPC.DesignPrinciples
+{
+    public class PaymentAccount
+    {
+        public PaymentAccount(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; }
+        public string CustomerName { get; set; }
+        public float Income { get; set; }
+        public float Outcome { get; set; }
+        public float AllowedDebit { get; set; }
+
+        public bool IsDebit()
+        {
+            return Income - Outcome < 0;
+        }
+    }
+}
--- WPC/Program.cs
+++ WPC/Program.cs
@@ -1,11 +1,11 @@
 ﻿using WPC.DesignPrinciples;
 
-var service = new Class2();
+var service = new PaymentService();
 
 var customerId = 1;
 
-service.Func2(customerId, 500);
-if (service.Func1(customerId, 100))
+service.Fund(customerId, 500);
+if (service.Charge(customerId, 100))
     Console.WriteLine($"Customer {customerId} charged. Actual balance: {service.GetBalance(customerId)}");
 else
     Console.WriteLine($"Not enought founds on customer {customerId} acconut! Actual balance: {service.GetBalance(customerId)}");
