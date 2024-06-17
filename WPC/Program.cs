443665dd0343d50dc9016edb1b4e65cbf26f60e6
Paweł Łęczycki <pawel@msd.info.pl>
2024-06-17 10:22:30 +0200

    DesignPrinciples

---
+++ WPC/Program.cs
@@ -0,0 +1,11 @@
+﻿using WPC.DesignPrinciples;
+
+var service = new Class2();
+
+var customerId = 1;
+
+service.Func2(customerId, 500);
+if (service.Func1(customerId, 100))
+    Console.WriteLine($"Customer {customerId} charged. Actual balance: {service.GetBalance(customerId)}");
+else
+    Console.WriteLine($"Not enought founds on customer {customerId} acconut! Actual balance: {service.GetBalance(customerId)}");
