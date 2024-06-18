88708ff61fbfb47467c5d934e9cba46ea65ead16
Paweł Łęczycki <pawel@msd.info.pl>
2024-06-18 15:21:40 +0200

    PrivateClassData

---
+++ WPC/DesignPatterns/Structural/PrivateClassData/ClassData.cs
@@ -0,0 +1,27 @@
+﻿namespace WPC.DesignPatterns.Structural.PrivateClassData
+{
+    internal class ClassData
+    {
+        //private readonly int _intValue;
+        //private string StringValue { get; }
+
+        private PrivateClassData _privateClassData;
+
+        public ClassData(int intValue, string stringValue)
+        {
+            //_intValue = intValue;
+            //StringValue = stringValue;
+
+            _privateClassData = new PrivateClassData(intValue, stringValue);
+        }
+
+        public void DoSth()
+        {
+            //_intValue = 0;
+            //StringValue = "";
+
+            _privateClassData.GetStringValue();
+            _privateClassData.GetIntValue();
+        }
+    }
+}
