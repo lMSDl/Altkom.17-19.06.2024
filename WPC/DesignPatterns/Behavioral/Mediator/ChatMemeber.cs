2d36b20efe9698eed6dc2d12c1d229dd59181653
Paweł Łęczycki <pawel@msd.info.pl>
2024-06-19 11:27:15 +0200

    Mediator

---
+++ WPC/DesignPatterns/Behavioral/Mediator/ChatMemeber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.DesignPatterns.Behavioral.Mediator
+{
+    internal abstract class ChatMemeber
+    {
+
+        public string Nick { get; set; }
+        private IMediator? mediator;
+        public IMediator Mediator
+        {
+            get => mediator;
+            set
+            {
+                if (mediator != null)
+                    mediator.Quit(this);
+                mediator = value;
+                if (mediator != null)
+                    mediator.Join(this);
+            }
+        }
+
+        public abstract void Receive(string from, string message, bool isPrivate);
+    }
+}
---
+++ WPC/DesignPatterns/Behavioral/Mediator/IMediator.cs
@@ -0,0 +1,10 @@
+﻿namespace WPC.DesignPatterns.Behavioral.Mediator
+{
+    internal interface IMediator
+    {
+        void Join(ChatMemeber member);
+        void Quit(ChatMemeber member);
+        void Send(string from, string message);
+        void Send(string from, string to, string message);
+    }
+}
